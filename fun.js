f7258c5ff5f856d185d4f311606da6ab922267ff
Abdul1678 <abdulahihussein345@gmail.com>
2025-04-08 01:30:42 +0000

    Push my index.html to the main branch

--- fun.js
+++ fun.js
@@ -1,1 +1,20 @@
+const PREPARATION_MINUTES_PER_LAYER = 2;
+const EXPECTED_MINUTES_IN_OVEN = 40;
 
+
+function remainingMinutesInOven(actualMinutesInOven) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+
+}
+console.log(remainingMinutesInOven(30));
+
+function preparationTimeInMinutes(numberOfLayers) {
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
+}
+console.log(preparationTimeInMinutes(3));
+
+function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+
+}
+console.log(totalTimeInMinutes(3, 20)); 
